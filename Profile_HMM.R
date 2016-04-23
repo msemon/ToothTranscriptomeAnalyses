b106a8a6b68a9485e26f449b607260a8cfc71e23
Laurent Gueguen <laurent.gueguen@univ-lyon1.fr>
2016-04-24 01:17:28 +0200

    comments on HMM

--- Profile_HMM.R
+++ Profile_HMM.R
@@ -1,12 +1,19 @@
-# TITRE 
+# Gaussian Hidden Markov Modeling of absolute differences in gene
+# expression between upper and lower teeth, for differentially
+# expressed genes.
 
-#http://www.r-bloggers.com/rstudio-and-github/
 
 library(RHmm)
 library(DESeq2)
 library(plyr)
 
-setwd("/home/msemon/Documents/PapierSouris/RSCRIPT_FOR_GITHUB/")
+
+###############################################
+##
+## Setting the data for HMM analysis
+##
+###############################################
+
 
 # Reading the data, using DESEq2 to get normalised (rld) values
 counts <- read.table("TabCountsMouse",sep="\t",h=T,row.names="id")
@@ -24,14 +31,19 @@ rsd <- rlogTransformation(dds)
 datarsd <- assay(rsd)
 
 
-# Getting genes that are differentially expressed between upper and lower over the whole time series
+# Getting genes that are differentially expressed between upper and
+# lower over the whole time series
+
 design(dds) = ~ tooth
 dds <- DESeq(dds)
 res <- results(dds, contrast=c("tooth","up","lo"))
 res01 <- res[res$padj<0.1 & !is.na(res$padj),]
 
 
-# Getting genes that are differentially expressed between upper and lower at each time points (taking consecutive timepoints as replicates)
+# Getting genes that are differentially expressed between upper and
+# lower at each time point (taking consecutive timepoints as
+# replicates)
+
 getDEperPairOfTimePoints <-function(i)
 {
 colData <- data.frame(std=rep(c(i,i+0.5),each=2),tooth=rep(c("lo","up"),2))
@@ -46,18 +58,30 @@ return(row.names(res01))
 
 listeDEPairOfTimePoints=sapply(seq(14.5,17.5,by=0.5),function(time){getDEperPairOfTimePoints(time)})
 
-# Merging the lists of DE genes, either over the whole time serie, or per time point
+# Merging the lists of DE genes, either over the whole time series, or
+# per time point
+
 listeAllDE=unique(c(unlist(listeDEPairOfTimePoints),row.names(res01)))
 
 # Taking only genes that are in these list of DE genes
+
 datarsdDE=datarsd[listeAllDE,]
-                  
+
+
+######################################################
+##
 ## Making the HMM Analysis on allgenes 
+##
+######################################################
+
 # get the columns for up and low data
+
 x <- row.names(colData)[colData$tooth=="up"]
 y <- row.names(colData)[colData$tooth=="lo"]
 
-# We are working on the differential between upper and lower tooth, per timepoint. 
+# We are working on the absolute value of the differential between
+# upper and lower tooth, per timepoint.
+
 diffexpr=data.frame(datarsdDE[,x]-datarsdDE[,y])
 names(diffexpr)=c(paste("stage",seq(14.5,18,by=0.5),sep=""))
 
@@ -70,9 +94,29 @@ diffexpr1=diffexpr[nb==16&dif==0,]
 diffexpr2Split=dlply(diffexpr1,1,function(x){abs(as.numeric(x))})
 names(diffexpr2Split)=row.names(diffexpr1)
 
-threshold=0.8
+
+# Over all time points, we model these differences through two normal
+# distributions, one for high differences ("HI") and one for medium or
+# small differences ("ME"), and we model the transitions between these
+# states HI and ME in an HMM.
+
+
+# HMMFit optimizes this modeling on all the data, with an EM
+# optimization (Baum-Welch algorithm).
 
 hmm2statesNormal <- HMMFit(obs=diffexpr2Split, nStates=2, dis="NORMAL")
+
+# We check that both states are separate (ie the mean and var of
+# Distribution parameters), and state 1 stands for "HI" differences.
+
+hmm2statesNormal$HMM
+
+# Given this optimized HMM, we compute, for each gene, the a
+# posteriori probability of each state on each time point. Then, for
+# each gene, we set as "HI" all time points for which the a posteriori
+# probability of state "HI" is above a given threshold.
+
+threshold=0.8
 
 ls=lapply(diffexpr2Split,function(d){
   fb=data.frame(forwardBackward(hmm2statesNormal, d)$Gamma)
@@ -86,6 +130,8 @@ lls=t(data.frame(ls))
 reshmm2statesNormal=data.frame(lls)
 names(reshmm2statesNormal)=seq(14.5,18,by=0.5)
 
+# We count all types of transitions between and within states "HI" and
+# "ME"
 
 ls=sapply(1:7,function(s){
   table(paste(reshmm2statesNormal[,s],reshmm2statesNormal[,(s+1)],sep="->"))
@@ -95,7 +141,7 @@ names(shifts2StatesNormal)=c("14.5->15","15->15.5","15.5->16","16->16.5","16.5->
 
 
 pdf("dotchart_HMMvalabs_DEgenes.pdf")
-dotchart(as.matrix(shiftsNormal_Tot),col=rainbow(4))
+dotchart(as.matrix(shifts2StatesNormal),col=rainbow(4))
 dev.off()
 
 
