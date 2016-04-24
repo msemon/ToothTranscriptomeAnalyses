66009f920c27deed856035af6881e8055aa035db
msemon <marie.semon@ens-lyon.fr>
2016-04-24 20:43:46 +0200

    Complete the document for cusp proportion modelling

--- Model_Cusp_Prop.R
+++ Model_Cusp_Prop.R
@@ -11,6 +11,7 @@ library(ade4)
 
 
 # Reading the data, using DESEq2 to get normalised (rld) values
+
 counts <- read.table("TabCountsMouse",sep="\t",h=T,row.names="id")
 counts <- counts[1:(nrow(counts)-5),]
 timepoints <- rep(seq(14.5,18,by=0.5),each=2)
@@ -40,8 +41,10 @@ pcaplot <- function(p, colData, decal=0.1, nm1="Comp1",  nm2="Comp2",  axis1=1, 
   text(p$li[,axis1]-decal, p$li[,axis2]-decal, lab=paste(colData[,1],colData[,2]),offset=1,cex=1)
 }
 
+# Daw the samples on the map of the 2 first component
 pcaplot(p,colData)
 
+# Daw the position of the samples on the map of the first and third principal component
 pcaplot(p,colData,nm1="Comp1",  nm2="Comp3",  axis1=1, axis2=3)
 
 
@@ -74,10 +77,14 @@ addcusp=function(nx,ny,nr)
 lnx <- list(2,0,2,0,3,c(1,3),1)
 lny <- list(1,0,2,0,2,c(2,1),1)
 contrasts <- matrix(0,nrow=3,ncol=2)
+
 # Make the cusps grow of one unit per time point
 llow <- lapply(1:8,function(i){addcusp(lnx[i],lny[i],nr=2)})
+
 # llow contains the size and position of cusps at each time point
 slow <- lapply(llow,sum)
+# the proportion of maturation is simply the sum of the size of cusps divided by the sum of the sizes if all cusps 
+# had grown to completion
 slowp <- unlist(slow)/(8*6)
 
 # Each tooth germ is a grid. Upper tooth has 3 rows of cusps, each containing 3 cusps (actually just 8 cusps are patterned ultimately)
@@ -88,14 +95,19 @@ lup <- lapply(1:8,function(i){addcusp(lnx[i],lny[i],nr=3)})
 slup <- lapply(lup,sum)
 slupp <- unlist(slup)/(8*8)
 
+###############################################
+##
+## Compare the heterochronies observed with PCA on the first principal component, and the difference of cusp proportion from the model
+##
+###############################################
 
-# Compare the heterochrony observed with PCA on the first principal component, and the difference of cusp proportion from the model
-plot(seq(14.5,18,by=0.5),slowp-slupp,pch=20,col="grey",type="h",ylab="heterochrony cusp prop model")
 
+# Heterochronies from the model of cusp propotion
+plot(seq(14.5,18,by=0.5),slowp-slupp,pch=20,col="grey",type="h",ylab="heterochrony cusp prop model",xlab="stages")
+# Heterocrhonies from the first axis of the PCA
 delta=abs(diff(p$li[,1])[seq(1,15,by=2)])
-plot(seq(14.5,18,by=0.5),delta,pch=20,col="grey",type="h",ylab="heterochrony PCA Comp1")
-
-
+plot(seq(14.5,18,by=0.5),delta,pch=20,col="grey",type="h",ylab="heterochrony PCA Comp1",xlab="stages")
+# Comparing heterochronies from the model and the PCA
 plot(slowp-slupp,delta,pch=20,xlab="deltamodel",ylab="deltaPCA")
 text(slowp-slupp,delta,seq(14.5,18,by=0.5))
 
@@ -104,50 +116,40 @@ text(slowp-slupp,delta,seq(14.5,18,by=0.5))
 clow=c(1,1,2,2,3,5,6,6)
 cup=c(1,1,1,3,3,4,6,8)
 
-
-pdf("maturationsUPLO.pdf")
-plot(slowp,slupp,pch=20,xlab="maturation LO",ylab="maturation UP",ylim=c(0,0.6),xlim=c(0,0.6))
+# Comparing the advancement of maturantion in upper compared to lower tooth. Upper is delayed.
+plot(slowp,slupp,pch=20,xlab="model cusp proportion lower",ylab="model cusp proportion upper",ylim=c(0,0.6),xlim=c(0,0.6))
 abline(b=1,a=0)
 text(slowp-slupp,delta,seq(14.5,18,by=0.5))
 abline(lm(slupp~slowp),col="red")
-dev.off()
 
 
 
-nbup=cbind(pos=unlist(slup),neg=c(64-unlist(slup)))
-glmup=glm(nbup~p$li[colData$tooth=="up",1],family="binomial")
+###############################################
+##
+## Correlate PCA on the first principal component, and cusp proportion from the model, using glm
+##
+###############################################
 
-nblo=cbind(pos=unlist(slow),neg=c(48-unlist(slow)))
-glmlo=glm(nblo~p$li[colData$tooth=="lo",1],family="binomial")
 
+# Generalised linear model for the upper and lower tooth, to fit the cusp prop estimated from the model and PCA1
 nbtot=data.frame(rbind(nbup,nblo))
 nbtot$type=c(rep("up",8),rep("lo",8))
 nbtot$pca=c(p$li[colData$tooth=="up",1],p$li[colData$tooth=="lo",1])
-glmtot=glm(cbind(nbtot$pos,nbtot$neg)~nbtot$pca+nbtot$type,family="binomial")
-
-
-glmpca=glm(cbind(nbtot$pos,nbtot$neg)~nbtot$pca,family="binomial")
-glmnull=glm(cbind(nbtot$pos,nbtot$neg)~1,family="binomial")
+glmpcatooth=glm(cbind(pos,neg)~pca+type,family="binomial",data=nbtot)
+glmpca=glm(cbind(pos,neg)~pca,family="binomial",data=nbtot)
+glmnull=glm(cbind(pos,neg)~1,family="binomial",data=nbtot)
 coef(summary(glmpca))[,4]
 1-logLik(glmpca)/logLik(glmnull)
-# 'log Lik.' 0.6967879 (df=2)
+# 'log Lik.' 0.7096039 (df=2)
+1-pchisq(logLik(glmpca)-logLik(glmnull),df=1)
+# PCA is a good predictor of cusp proportions
+1-logLik(glmpcatooth)/logLik(glmnull)
+1-pchisq(logLik(glmpcatooth)-logLik(glmpca),df=1)
+# the relation PCA and cusp proportions is the same for upper and lower
 
-pchisq(137.5032-3.0693,df=1)
-
-glmlo=glm(cbind(pos,neg)~pca,family="binomial",data=nbtot[nbtot$type=="lo",])
-glmnull=glm(cbind(pos,neg)~1,family="binomial",data=nbtot[nbtot$type=="lo",])
-coef(summary(glmpca))[,4] --> 3.460409e-22 
-1-logLik(glmpca)/logLik(glmnull)
-# 'log Lik.' 0.4154333 (df=2)
-
-1-glmlo$deviance/glmlo$null.deviance
-0.9774495
-
-pdf("PCA_modeleGLMlo.pdf")
-plot(nbtot$pca[nbtot$type=="lo"],nbtot$pos[nbtot$type=="lo"]/(nbtot$neg[nbtot$type=="lo"]+nbtot$pos[nbtot$type=="lo"]),col=ifelse(nbtot$type[nbtot$type=="lo"]=="up","black","grey"),pch=20,ylim=c(0,0.6),ylab="prop model",xlab="pca axis 1")
-x = seq(-40,40)
-lines(x,predict(glmlo,newdata=data.frame(pca=x),type="response"),lwd=2,col="yellow")
-dev.off()
+plot(nbtot$pca,nbtot$pos/(nbtot$neg+nbtot$pos),col=ifelse(nbtot$type=="up","black","grey"),pch=20,ylim=c(0,0.6),ylab="prop model",xlab="PCA axis 1")
+x = seq(-30,30)
+lines(x,predict(glmpca,newdata=data.frame(pca=x),type="response"),lwd=2,col="red")
 
 
 
